004d7904b117fabba715d02b0895f91c6e152bda
marchaem <emmanuel.marchalfranis@gmail.com>
2019-01-05 20:53:12 +0100

    maj

--- src/components/Donkey/Donkey.vue
+++ src/components/Donkey/Donkey.vue
@@ -1,12 +1,11 @@
 <template>
 	<div>
-		<img src="/../static/funny_donkey.jpg" style="max-width: 100%; height: auto;" alt="Description">		
+		<img src="/../static/funny_donkey.jpg" >		
 	</div>
 </template>
 
 <script>
 	export default {
-
 		name: 'donkey',        
     }
 </script>
